c0827451012b093969b302d4dcbed0001fccbf28
nayanjeetk795-imsec <nayanjeetk795@gmail.com>
2026-06-05 10:51:35 +0530

    Add files via upload

---
+++ Lab_work/Q-5elecbillcalc.py
@@ -0,0 +1,19 @@
+# Electricity Bill Calculator
+
+units = int(input("Enter units consumed: "))
+
+if units <= 100:
+    bill = units * 5
+    category = "Low Consumption"
+
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+    category = "Medium Consumption"
+
+else:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+    category = "High Consumption"
+
+print("Units Consumed =", units)
+print("Total Bill = ₹", bill)
+print("Category =", category)

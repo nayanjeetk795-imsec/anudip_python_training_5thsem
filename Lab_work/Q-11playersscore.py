1a65051117a45e47a233a521c4bb5327a12cba3a
nayanjeetk795-imsec <nayanjeetk795@gmail.com>
2026-06-08 06:12:01 +0530

    Delete Lab_work directory

--- Lab_work/Q-11playersscore.py
+++
@@ -1,9 +0,0 @@
-# PLAYER SCORE
-player_score = []
-# input of score form user
-for i in range(11):
-    score = int(input("Enter the score of player {}: ".format(i + 1)))
-    player_score.append(score)
-# display the score of player
-print("\n----- Player Scores -----")
-print("score of 11 players: ", player_score)

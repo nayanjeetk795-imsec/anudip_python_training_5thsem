cba6f8b6e1dabcc6d48c8a260f23e6e8b0a0d7a0
nayanjeetk795-imsec <nayanjeetk795@gmail.com>
2026-06-08 10:53:22 +0530

    cricket tournament statistic

---
+++ labwork/8th_june_dictionary/cricket_tournament_statistics.py
@@ -0,0 +1,19 @@
+Cricket Tournament Statistics
+Problem Statement
+Runs scored by players in a tournament:
+runs = { "Virat": 645, "Rohit": 512, "Gill": 698, "Rahul": 435, "Hardik": 278, "Pant": 534, "Surya": 389, "Jadeja": 301, "Iyer": 455, "KL": 410 }
+Tasks
+1.
+Display players scoring more than 500 runs.
+2.
+Find the Orange Cap winner.
+3.
+Find the lowest scorer.
+4.
+Calculate total runs scored.
+5.
+Create a list of players scoring below 400.
+6.
+Count players scoring between 400 and 600 runs.
+Sample Output
+Players Scoring More Than 500 Runs: Virat Rohit Gill Pant Orange Cap Winner: Gill (698) Lowest Scorer: Hardik (278) Total Tournament Runs: 4657 Players Scoring Below 400: ['Hardik', 'Surya', 'Jadeja'] Players Between 400 and 600 Runs: 5
